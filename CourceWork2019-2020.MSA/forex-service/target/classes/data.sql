16cd8ed203de5d4bf9db8072d8affb9e061fcf9a
Sofia Kovalchuk <36841164+CheshirLvova@users.noreply.github.com>
2020-05-25 22:40:10 +0300

    MSA using Spring Boot
    Development of MSA with
    using Java technology

---
+++ CourceWork2019-2020.MSA/forex-service/target/classes/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10001,'USD','UAH',26,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10002,'EUR','UAH',29,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10003,'GBP','UAH',33,0);
